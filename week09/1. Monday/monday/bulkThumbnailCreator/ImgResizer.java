5edae399c0f2e4fc82d08770547919d981cb1441
Partsaleva <m_partsaleva@abv.bg>
2016-03-24 21:22:04 +0200

    Added solution for thumbnail creator

--- week09/1. Monday/monday/bulkThumbnailCreator/ImgResizer.java
+++ week09/1. Monday/monday/bulkThumbnailCreator/ImgResizer.java
@@ -5,50 +5,74 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import javax.imageio.ImageIO;
 
-public class ImgResizer {
-	public static void main(String[] args) {
-		//Path p=Paths.get("/home/partsaleva/Documents/img.jpg");
-		try {
-			resizeAndStore("-r","/home/partsaleva/Documents",150,150);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+public class ImgResizer extends Thread{
+	public static void main(String[] args) {		
+		ImgResizer tr1=new ImgResizer();
+		tr1.setDaemon(true);
+		tr1.run();
 		
-
 	}
-	// /home/partsaleva/Documents/img.jpg
 	
-	   public static void resizeAndStore(String command,String directoryName,int width,int height) throws IOException{
-	        File directory = new File(directoryName);
+	   private void resizeAndStore(String command,String directoryN,int width,int height) throws IOException, InterruptedException{
+	        File directory = new File(directoryN);
 	        //get all the files from a directory
 	        File[] fList = directory.listFiles();
-	        createThumbnailsDirectory(Paths.get(directoryName));
+	        //create thumbnails directory
+	        createThumbnailsDirectory(Paths.get(directoryN));
+	        
 	       for (File file : fList){
 	            if (file.isFile() ){
-	                System.out.println(file.getName());
-	            	//resize(Paths.get(file.getPath()),width,height);
-					//						
-	        			
+	                 System.out.println(file.getName());
+	            	//if file is picture resize and make copy
+	                if (isPicture(file)) {
+	                	Thread tr1=new Thread(new Runnable() {		
+							@Override
+							public void run() {
+								try {
+									resize(Paths.get(file.getPath()),width,height);
+								} catch (IOException e) {
+									e.printStackTrace();
+								}								
+							}
+						});
+	                	tr1.start();
+		            	tr1.join();
+					}
 	        		
 	            }
-	            else if(file.isDirectory() && command=="-r"){
+	            //if is directory and we want to do the same for subfolders 
+	            else if(file.isDirectory() && command=="-r" && !file.getName().equals("thumbnails") ){
 	            	System.out.println(file.getName());
-	        		createThumbnailsDirectory(Paths.get(directoryName+file.getName()));        		
-	            	   // resizeAndStore(command,file.getAbsolutePath(),width,height);
+	            	Thread tr2=new Thread(new Runnable() {			
+						@Override
+						public void run() {
+							try {
+								createThumbnailsDirectory(Paths.get(file.getAbsolutePath()));
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+							
+						}
+					});
+	            	tr2.start();
+	            	tr2.join();
+	            	resizeAndStore(command,file.getAbsolutePath(),width,height);
 	            }
 	        }
 	    }
-	   
-	static boolean isPicture(File file){
-		String[] fileName=file.getName().split("\\.");
-		return fileName[1]=="jpg" || fileName[1]=="png";
-	}  
-	private static void resize(Path path, int width,int height) throws IOException{
+	 //check if file name ends with jpg or png (we can add more)  
+	private boolean isPicture(File file){
+		return file.toString().toLowerCase().endsWith("jpg") || 
+				file.toString().toLowerCase().endsWith("png") ;
+	} 
+	
+	//resize and write resized file
+	private void resize(Path path, int width,int height) throws IOException{
 		BufferedImage original=ImageIO.read(new File(path.toString()));
 		int type = original.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : original.getType();
 		
@@ -58,24 +82,34 @@ public class ImgResizer {
 		g.dispose();
 		File f=moveFile(path);
 		ImageIO.write(resized, "jpg", f);	
-		//return f;
 	}
 	
-	private static String createThumbnailsDirectory(Path path){			
-			
-			String result = path+ "/thumbnails";
-			
-			new File(result).mkdirs();
-			return result;
+	//add subfolder for the smaller picture copies
+	private void createThumbnailsDirectory(Path path) throws IOException{				
+			Path thumbnailsDir = Paths.get(path.toString(), "thumbnails");
+			Files.createDirectories(thumbnailsDir);
 	}	
 	
 	//helper function to save resized file to thumbnail folder
-	private static File moveFile(Path path){
+	private File moveFile(Path path){
 		File file=path.toFile();
 		String p=file.getParent();
 		String[] fileName=file.getName().split("\\.");
 		return new File(p+"/thumbnails/"+fileName[0]+"_resized."+fileName[1]);
 	
+	}
+
+	@Override
+	public void run() {
+		try {
+			resizeAndStore("-r","/home/partsaleva/Documents",150,150);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 }
