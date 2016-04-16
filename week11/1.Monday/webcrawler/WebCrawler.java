d1fd4aab26fa16216bc49392d4b4847c915adf9f
Partsaleva <m_partsaleva@abv.bg>
2016-04-16 10:00:01 +0300

    need repair

--- week11/1.Monday/webcrawler/WebCrawler.java
+++ week11/1.Monday/webcrawler/WebCrawler.java
@@ -16,11 +16,13 @@ import java.util.regex.Pattern;
 public class WebCrawler {
 
 	public static void main(String[] args) throws IOException {
-		URL url=new URL("http://ebusiness.free.bg/");
-		String text="Револвираща";
-		String webText=getText(url);			
+		URL url=new URL("http://www.kolonkata.info/search/label/%D0%91%D0%B5%D0%B7%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE");
+		String text="16 април";
+		String webText=getText(url);
+		System.out.println(webText);
 		search(url, text);
 		System.out.println(pagesToVisit);
+		
 
 	}
 
@@ -29,19 +31,20 @@ public class WebCrawler {
 	  
 	public static void search(URL url, String text) throws IOException{
 		URL currentUrl;
-		if(pagesToVisit.isEmpty()){
-              currentUrl = url;
-            //  System.out.println(currentUrl.toString());
-              pagesToVisit.addAll(getAllLinks(getText(currentUrl)));
-        }
-		else{
-              currentUrl = new URL(nextUrl());
-        }
-		if(containText(getText(currentUrl),text)){
-			pagesVisited.add(currentUrl.toString());
- 			System.out.println("FOUND IN " + currentUrl.toString().toUpperCase());
-		}
-		//pagesToVisit.addAll(getAllLinks(getText(currentUrl)));
+	
+			if(pagesToVisit.isEmpty()){
+	              currentUrl = url;
+	              System.out.println(currentUrl.toString());
+	              pagesToVisit.addAll(getAllLinks(currentUrl,getText(currentUrl)));
+	        }
+			else{
+	              currentUrl = new URL(nextUrl());
+	        }
+			if(containText(getText(currentUrl),text)){
+				pagesVisited.add(currentUrl.toString());
+	 			System.out.println("FOUND IN " + currentUrl.toString().toUpperCase());
+			}
+		
 		
 	}
 	
@@ -52,14 +55,14 @@ public class WebCrawler {
 		return nextUrl;
 	}
 
-	private static List<String> getAllLinks(String content) {
-		 String regex = "<a.*?href=\"((?!javascript).*?)\".*?>";
+	private static List<String> getAllLinks(URL url, String content) {
+		    String regex = "<a.*?href=\"((?!javascript).*?)\".*?>";
 	        Pattern pattern = Pattern.compile(regex,Pattern.CASE_INSENSITIVE|Pattern.DOTALL);
 	        Matcher matcher = pattern.matcher(content);
 	        while (matcher.find()) {
-	        	String w = matcher.group(1).replace("../", "");
-	            if (!pagesToVisit.contains(w)) {
-	            	pagesToVisit.add(w);
+	        	String w = matcher.group(1);
+	            if (!pagesToVisit.contains(w)) {            
+	            		pagesToVisit.add(w);				
 	            }
 	        }
 	        return pagesToVisit;
@@ -71,13 +74,13 @@ public class WebCrawler {
 	        String line;
 	        while ((line = in.readLine()) != null){
 	        	b.append(line);
+	        	b.append(System.getProperty("line.separator"));
 	        }        
         }
 		return b.toString();
 	   }
 	
-	private static boolean containText(String urlText, String text){
-		
+	private static boolean containText(String urlText, String text){		
 		 System.out.println("Searching for " + "'"+text + "'"+"...");	       
 	     return urlText.toLowerCase().contains(text.toLowerCase());
 	}
