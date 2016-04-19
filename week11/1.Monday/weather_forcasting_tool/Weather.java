558a5fd53568148bb86dd4d6d8e1c6f859119c56
Partsaleva <m_partsaleva@abv.bg>
2016-04-19 10:21:34 +0300

    Add return weather in current location

---
+++ week11/1.Monday/weather_forcasting_tool/Weather.java
@@ -0,0 +1,12 @@
+package weather_forcasting_tool;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.codehaus.jettison.json.JSONObject;
+
+public class Weather {
+
+	
+}
--- week11/1.Monday/weather_forcasting_tool/WeatherForcastingTool.java
+++ week11/1.Monday/weather_forcasting_tool/WeatherForcastingTool.java
@@ -1,10 +1,34 @@
 package weather_forcasting_tool;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
 public class WeatherForcastingTool {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException, JSONException {
+		WeatherForcastingTool tool = new WeatherForcastingTool();
+		URL url=new URL("http://api.openweathermap.org/data/2.5/find?q=Sofia,bg&appid=5c557da33209735e6993f7f1a091851d");
+        JSONObject o =tool.getJson(url);
+        System.out.println(o);
 
 	}
 
+	private JSONObject getJson(URL url) throws IOException, JSONException {
+		URLConnection connection = url.openConnection();
+		try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+			String inputLine;
+			StringBuilder b = new StringBuilder();
+			while ((inputLine = in.readLine()) != null) {
+				b.append(inputLine);
+			}
+			return new JSONObject(b.toString());
+		}
+
+	}
 }
