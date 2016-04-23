b22ef0adb6f5dfb8f516a40ac05acadfd76839fc
Partsaleva <m_partsaleva@abv.bg>
2016-04-23 12:07:01 +0300

    maded some changes

---
+++ NewsAgregator/src/service/Agregator.java
@@ -0,0 +1,49 @@
+package service;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
+
+public class Agregator {
+
+	public String getRss(String rss){
+		URL url;
+		StringBuilder sb=new StringBuilder();
+		
+		try {
+			url = new URL(rss);
+			HttpURLConnection httpcon = (HttpURLConnection)url.openConnection();
+			// Reading the feed
+			SyndFeedInput input = new SyndFeedInput();
+			SyndFeed feed = input.build(new XmlReader(httpcon));
+			@SuppressWarnings("unchecked")
+			List<SyndEntry> entries = feed.getEntries();
+			for (SyndEntry syndEntry : entries) {
+				sb.append("<h1>" + syndEntry.getTitle() + "</h1>");
+				sb.append("<br/>");
+				String desc = syndEntry.getDescription().getValue();
+				if (desc.endsWith("/>") && desc.length() > 100) {
+					desc = desc.substring(0, desc.length()-10);
+				}
+				sb.append("<div>"+desc + "<a href=" + syndEntry.getLink() + ">" + "more</a></div><hr><br/>");	
+			
+			}
+			
+		} catch (IllegalArgumentException | FeedException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return sb.toString();
+
+	}
+}
---
+++ NewsAgregator/src/servlets/News.java
@@ -0,0 +1,43 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.Agregator;
+
+/**
+ * Servlet implementation class News
+ */
+@WebServlet("/news")
+public class News extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Agregator agr=new Agregator();
+    /**
+     * Default constructor. 
+     */
+    public News() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		request.setAttribute("news", agr.getRss("http://www.dnevnik.bg/rssc/?rubrid=1657"));
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
