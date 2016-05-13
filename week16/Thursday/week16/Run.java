e560c261920cef779f30a94168703f25d25c1a63
Partsaleva <m_partsaleva@abv.bg>
2016-05-13 08:25:58 +0300

    Made Zero singleton

--- week16/Thursday/week16/Run.java
+++ week16/Thursday/week16/Run.java
@@ -3,10 +3,10 @@ package week16;
 public class Run {
 
 	public static void main(String[] args) {
-		System.out.println(new Zero().toString());
-		System.out.println(new Succ(new Succ(new Zero())).toInt());
-		System.out.println(new Succ(new Zero()).toString());
-		System.out.println(new Succ(new Succ(new Zero())).toString());
+		System.out.println(Zero.getInstance().toString());
+		System.out.println(new Succ(new Succ(Zero.getInstance())).toInt());
+		System.out.println(new Succ(Zero.getInstance()).toString());
+		System.out.println(new Succ(new Succ(Zero.getInstance())).toString());
 	}
 
 }
