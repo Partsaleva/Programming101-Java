c34058256bcc9f4e6b45bdbb57160a84d59999a8
Partsaleva <m_partsaleva@abv.bg>
2016-05-25 20:38:29 +0300

    Merge Sort

--- week18/Tuesday/sortingAlgorithms/MergeSort.java
+++ week18/Tuesday/sortingAlgorithms/MergeSort.java
@@ -1,52 +1,49 @@
 package sortingAlgorithms;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class MergeSort {
 
 	public static void main(String[] args) {
-		List<Integer>a=Arrays.asList(2,5,9,7,1,14,56,11,4,7);
-		System.out.println(mergeSort(a, 0, a.size()-1));
-		
-
-	}
-
-	
-
-	public static List<Integer> mergeSort(List<Integer> A , int begin, int end){
-		if(end - begin < 2){
-			 return A; 
-		}                                                  
-	    
-	    int middle = (end + begin) / 2;              
-	    mergeSort(A, begin,  middle);  
-	    mergeSort(A, middle, end);  
-	    return merge(A, begin, middle, end);
-	    
-	}
-	
-	
-	public static List<Integer> merge(List<Integer> A , int begin, int middle, int end){
-		int iBegin=begin;
-		int iMid=middle;
-		List<Integer> B=new ArrayList<Integer>();
-		
-		for (int i = iBegin; i < end; i++){
-			
-			if (iBegin < iMid && (A.get(iMid) >= A.get(end) || A.get(iBegin)< A.get(iMid))) {
-				
-				B.add(A.get(iBegin));
-				iBegin++;
-				
-			}else{
-				
-				B.add(A.get(iMid));
-				iMid++;
-			}
+		int[] arr  = new int[]{2, 5, 9, 7, 1, 14, 56, 11, 4, 7};
+		mergeSort(arr, 0, arr.length - 1);
+		for (int i : arr) {
+			System.out.print(i + " ");
 		}
-		System.out.println(B);
-		return B;
+
+	}
+
+	public static void mergeSort(int[] arr, int low, int high) {
+		if (low < high) {
+			int middle = (low + high) / 2;
+			mergeSort(arr, low, middle);
+			mergeSort(arr, middle + 1, high);
+			merge(arr, low, middle, high);
+		}
+
+	}
+
+	public static void merge(int[] arr , int low, int middle, int high){
+		int[] temp=Arrays.copyOf(arr, arr.length);
+	
+		int i = low;
+		int j = middle+1;
+		int k=low;
+		while(i<=middle && j <= high){
+			if (temp[i] <= temp[j]) {
+				arr[k]=temp[i];
+				++i;
+			} else {
+				arr[k]=temp[j];
+				++j;
+			}
+			++k;
+		}
+		
+		while(i <= middle){
+			arr[k]=temp[i];
+			++k; ++i;
+		}
+
 	}
 }
